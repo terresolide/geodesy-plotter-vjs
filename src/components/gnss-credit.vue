e90750033ee425264b983d098ddbb2f192808555
epointal <elisabeth.pointal@free.fr>
2023-09-14 13:02:36 +0200

    #67 composant pour credit

---
+++ src/components/gnss-credit.vue
@@ -0,0 +1,32 @@
+<template>
+  <span v-if="credit" class="gnss-credit">
+   <span class="credit-link tooltip-show" title="copyright/credit">
+    <font-awesome-icon icon="fa-solid fa-copyright" />
+    </span>
+    <div class="gdm-tooltip">
+       {{credit}}
+    </div>
+  </span>
+</template>
+<script>
+export default {
+  name: 'GnssCredit',
+  props: {
+    credit: {
+	    type: String,
+	    default: null
+    }
+  }
+}
+</script>
+<style scoped>
+.gnss-credit {
+  position:relative;
+}
+.gnss-credit .gdm-tooltip {
+  color:#333;
+}
+.credit-link {
+  cursor: pointer;
+}
+</style>
