4ab403a0de504c75fcc27f9de8e078a9626dfa21
epointal <elisabeth.pointal@free.fr>
2024-05-31 15:02:37 +0200

    ForM@Ter -> FormaTerre

--- src/App.vue
+++ src/App.vue
@@ -25,7 +25,7 @@
 	
 			 <div  v-if="$route.name !== 'home'" id="page" style="position:absolute;z-index:2;">
 			
-			     <div class="gnss-attribution">Component by <a href="https://www.poleterresolide.fr" title="ForM@Ter"><img :src="require('./assets/img/favicon.png').default" width="24" /></a></div>
+			     <div class="gnss-attribution">Component by <a href="https://www.poleterresolide.fr" title="FormaTerre"><img :src="require('./assets/img/favicon.png').default" width="24" /></a></div>
 			     <router-view></router-view>
 			 </div>
 		 </div>
